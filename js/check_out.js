d79409e1f509c0268b9effaddb388619bfe6f930
NguyenMinhThien97 <minhthiennguyen1997@gmail.com>
2020-12-24 02:22:10 +0700

    paypal

--- js/check_out.js
+++ js/check_out.js
@@ -31,14 +31,23 @@
     $("#btnThanhToan").click(function() {
         var account = JSON.parse(localStorage.getItem("user"));
         if (account != null) {
-            checkTonKho(account);
+            var payment = $('#payment').is(":checked");
+            var paypal = $('#paypal').is(":checked");
+            if (!payment && !paypal || payment && paypal) {
+                alert("Vui lòng chọn một phương thức thanh toán để tiếp tục!")
+            } else if (payment) {
+                thanhToanPayment(account);
+            } else {
+                thanhToanPaypal(account, removeFormatMoney($('#tongTien').text()));
+            }
+
         } else {
             alert("Vui lòng đăng nhập để thực hiện thanh toán!")
         }
     });
 })(jQuery);
 
-function checkTonKho(account) {
+function thanhToanPayment(account) {
     $.ajax({
         type: "GET",
         contentType: "application/json",
@@ -85,4 +94,42 @@ function checkTonKho(account) {
             console.log("ERROR : ", e);
         }
     });
+}
+
+function thanhToanPaypal(account, tongTien) {
+    $.ajax({
+        type: "GET",
+        contentType: "application/json",
+        url: "http://localhost:8080/transaction/checkTonKho/" + account.idNguoiDung,
+        async: false,
+        crossDomain: true,
+        success: function(result) {
+            if (result == true) {
+                callPaypal(tongTien);
+            } else {
+                alert("Hiện tại có 1 số sản phẩm không còn đủ hàng để bán, bạn vui lòng kiểm tra lại số lượng sản phẩm trong giỏ hàng của bạn.")
+                window.location.replace("shoping-cart.html");
+            }
+        },
+        error: function(e) {
+            console.log("ERROR : ", e);
+        }
+    });
+}
+
+function callPaypal(tongTien) {
+    var formData = new FormData();
+    formData.append("price", tongTien);
+
+    var request = new XMLHttpRequest();
+    request.open("POST", "http://localhost:8080/paypal/pay", false);
+    request.onreadystatechange = function() {
+        if (request.readyState == XMLHttpRequest.DONE && request.status == 200) {
+            console.log(request);
+            location.replace(request.response);
+        } else {
+            alert("Error!");
+        }
+    };
+    request.send(formData);
 }
